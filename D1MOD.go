3dca3c499e8b39d2d972a4d5b760942277a21690
mrxD1MOD <99124744+mrxD1MOD@users.noreply.github.com>
2022-02-13 15:14:28 +0300

    Update D1MOD.go

--- D1MOD.go
+++ D1MOD.go
@@ -42,8 +42,6 @@ var (
 		"http://www.google.com/?q=",
 		"http://www.usatoday.com/search/results?q=",
 		"http://engadget.search.aol.com/search?q=",
-		"https://cloudfare.com",
-		"https://wordpress.org",
 		//"http://www.google.ru/?hl=ru&q=",
 		//"http://yandex.ru/yandsearch?text=",
 	}
